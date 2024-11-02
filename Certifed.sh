ebfb03275e2e4f514e461c04e98d45ca28ef89fa
koko <ko282ko82@gmail.com>
2024-11-03 01:10:08 +0530

    Update Certifed.sh

--- Certifed.sh
+++ Certifed.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm -rf vendor/extra
 git clone https://github.com/koko-07870/vendor_extra -b main vendor/extra
-cd vendor/extra
+rm -rf device/samsung/sm7125-common/common.mk
+cd device/samsung/sm7125-common
+wget -O common.mk https://raw.githubusercontent.com/koko-07870/device_samsung_sm7125-common/refs/heads/fourteen/common.mk
+cd -
+. build/envsetup.sh
+lunch aosp_a52q-ap2a-userdebug
 make CertifiedPropsOverlay
