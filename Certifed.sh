92fa23f56896608183c82f9d4e5bf7f4eb2c046f
koko <ko282ko82@gmail.com>
2024-11-03 01:21:14 +0530

    Update Certifed.sh

--- Certifed.sh
+++ Certifed.sh
@@ -5,6 +5,8 @@ rm -rf device/samsung/sm7125-common/common.mk
 cd device/samsung/sm7125-common
 wget -O common.mk https://raw.githubusercontent.com/koko-07870/device_samsung_sm7125-common/refs/heads/fourteen/common.mk
 cd -
+rm -rf packages/apps/FMRadio
+rm -rf vendor/qcom/opensource/libfmjni
 . build/envsetup.sh
 lunch aosp_a52q-ap2a-userdebug
 make CertifiedPropsOverlay
