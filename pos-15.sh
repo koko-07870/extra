cb8469fac57fc5321026d047e4c700bcee7876f4
koko <ko282ko82@gmail.com>
2024-11-12 13:43:06 +0530

    Update pos-15.sh

--- pos-15.sh
+++ pos-15.sh
@@ -19,10 +19,16 @@ git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos .repo/l
 rm -rf vendor/aosp/signing/keys
 git clone https://github.com/koko-07870/scripts -b tmp vendor/aosp/signing/keys
 
+# default wallpaper
+rm -rf vendor/aosp/overlay/common/frameworks/base/core/res/res/drawable-nodpi/default_wallpaper.png
+cd vendor/aosp/overlay/common/frameworks/base/core/res/res/drawable-nodpi
+wget -O default_wallpaper.png https://github.com/koko-07870/scripts/blob/pos/IMG_20241109_203223_173.png?raw=true
+cd -
+
 # Export
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
-echo "======= Export Done ======"
+echo "------ Export Done ------"
 
 source build/envsetup.sh
 
