deb358d3ad2f3aad22bd34dde6667e601207aa25
koko <ko282ko82@gmail.com>
2024-12-02 21:05:35 +0530

    Update a52q.sh

--- a52q.sh
+++ a52q.sh
@@ -2,23 +2,10 @@
 function compile()
 {
 source ~/.bashrc && source ~/.profile
-# Default directory where kernel is located in.
 KDIR=$(pwd)
 export KDIR
-# defconfig
 export CONFIG=pixelos-a52q_defconfig
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 15G
-TANGGAL=$(date +"%Y%m%d-%H")
-export ARCH=arm64
-export KBUILD_BUILD_HOST=linux-build
-export KBUILD_BUILD_USER="koko"
-clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r530567.git clang
-fi
-rm -rf anykernel
-# A function to regenerate defconfig.
-rgn() {
+function rgn() {
 	echo -e "\n\e[1;93m[*] Regenerating defconfig! \e[0m"
         if [ ! -d "${KDIR}/out" ]; then
         make $CONFIG
@@ -30,7 +17,16 @@ rgn() {
 	cp -rf "${KDIR}"/out/.config "${KDIR}"/arch/arm64/configs/vendor/$CONFIG
 	echo -e "\n\e[1;32m[✓] Defconfig regenerated! \e[0m"
 }
-# start building
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 15G
+TANGGAL=$(date +"%Y%m%d-%H")
+export ARCH=arm64
+export KBUILD_BUILD_HOST=linux-build
+export KBUILD_BUILD_USER="koko"
+clangbin=clang/bin/clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r530567.git clang
+fi
+rm -rf anykernel
 make O=out ARCH=arm64 vendor/$CONFIG
 PATH="${KDIR}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -62,4 +58,5 @@ cd ../
 fi
 }
 compile
+rgn
 zupload
