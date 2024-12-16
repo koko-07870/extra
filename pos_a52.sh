0991250050c3634ccb17a15dd837c4b0091ea1b6
koko-07870 <ko282ko82@gmail.com>
2024-12-17 00:16:41 +0530

    Update pos_a52.sh

--- pos_a52.sh
+++ pos_a52.sh
@@ -8,15 +8,15 @@ rm -rf packages/apps/ViPER4AndroidFX
 rm -rf vendor/extra
 rm -rf kernel/samsung
 
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+repo init -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs
 
-git clone https://github.com/koko-07870/local_manifests --depth 1 -b yaap .repo/local_manifests
+git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .repo/local_manifests
 
 # sync
 /opt/crave/resync.sh
 
-rm -rf vendor/yaap/signing/keys
-git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
+rm -rf vendor/aosp/signing/keys
+git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
 
 # Export
 export BUILD_USERNAME=koko-07870
@@ -25,6 +25,4 @@ echo "------ Export Done ------"
 
 source build/envsetup.sh
 
-breakfast a52q
-TARGET_BUILD_GAPPS=true
-m yaap
+breakfast a52q && make installclean && mka bacon -j$(nproc --all)
