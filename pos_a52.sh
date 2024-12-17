ee70c3e2c2288d54955ca978ee3302e4d66faa56
koko-07870 <ko282ko82@gmail.com>
2024-12-17 20:15:23 +0530

    Update pos_a52.sh

--- pos_a52.sh
+++ pos_a52.sh
@@ -10,19 +10,33 @@ rm -rf kernel/samsung
 
 repo init -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs
 
+# local_manifests
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .repo/local_manifests
 
 # sync
 /opt/crave/resync.sh
 
+# keys
 rm -rf vendor/aosp/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
 
 # Export
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
-echo "------ Export Done ------"
+echo "exported hostname"
 
+# start
 source build/envsetup.sh
 
 breakfast a52q && make installclean && mka bacon -j$(nproc --all)
+
+# gofile-upload
+FILE="out/target/product/a52q/PixelOS_a52q*.zip
+
+SERVER=$(curl -s https://api.gofile.io/servers | jq -r '.data.servers[0].name')
+
+LINK=$(curl -# -F "file=@$FILE" "https://${SERVER}.gofile.io/uploadFile" | jq -r '.data|.downloadPage') 2>&1
+
+echo "$LINK"
+
+echo
