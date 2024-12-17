2312d456ab780c3c7e9ceab2b7d0112e71653a55
koko-07870 <ko282ko82@gmail.com>
2024-12-17 17:45:33 +0530

    Create sign.sh

---
+++ sign.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/306bobby-android/crDroid-build-signed-script/main/create-signed-env.sh
+
+chmod +x create-signed-env.sh
+
+./create-signed-env.sh
