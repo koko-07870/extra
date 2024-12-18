a887164c401851452052440426bda465cc9431ca
koko-07870 <ko282ko82@gmail.com>
2024-12-19 01:37:31 +0530

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -10,5 +10,5 @@ git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
 source build/envsetup.sh
 export TARGET_BUILD_GAPPS=true
 lunch yaap_a52q-user
-m yaap -j$(nproc --all)
+make installclean && m yaap -j$(nproc --all)
 
