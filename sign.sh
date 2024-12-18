84618c38df321078d5256eab538b7f4525e42d1d
koko-07870 <ko282ko82@gmail.com>
2024-12-18 21:40:43 +0530

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
-
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
-
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
 
