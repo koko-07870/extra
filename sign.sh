4b9fc0d830e660199c0436ab8dd7f0650e8235bd
koko-07870 <ko282ko82@gmail.com>
2024-12-18 13:35:00 +0530

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
-
+rm -rf .repo/local_manifests
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
 
-# sync
 repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+
+rm -rf vendor/yaap/signing/keys
+git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
+
+source build/envsetup.sh
+export TARGET_BUILD_GAPPS=true
+lunch yaap_a52q-user
+m yaap -j $(nproc --all)
