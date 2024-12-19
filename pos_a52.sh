6960ca79a180635fc2047379a91fb82dfdcfa785
koko-07870 <ko282ko82@gmail.com>
2024-12-19 21:35:20 +0530

    Update pos_a52.sh

--- pos_a52.sh
+++ pos_a52.sh
@@ -20,6 +20,9 @@ git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .re
 rm -rf vendor/aosp/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
 
+# huh google
+rm -rf out/soong/*.glob*
+
 # Export
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
