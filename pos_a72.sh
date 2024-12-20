f496b02d0bbe8af4c0d6e73f042bc22f30eadd30
koko-07870 <ko282ko82@gmail.com>
2024-12-20 10:55:49 +0530

    Update pos_a72.sh

--- pos_a72.sh
+++ pos_a72.sh
@@ -10,19 +10,34 @@ rm -rf kernel/samsung
 
 repo init -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs
 
+# local_manifests
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a72 .repo/local_manifests
 
 # sync
 /opt/crave/resync.sh
 
+# keys
 rm -rf vendor/aosp/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
+
+# huh google
+rm -rf out/soong/*.glob*
 
 # Export
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
-echo "------ Export Done ------"
+echo "exported hostname"
 
+# start
 source build/envsetup.sh
 
 breakfast a72q && make installclean && mka bacon -j$(nproc --all)
+
+# gofile-upload
+rm -rf upload.sh
+
+wget -O upload.sh https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/refs/heads/master/upload.sh
+
+bash upload.sh out/target/product/a52q/PixelOS_a72q*.zip
+
+echo 
