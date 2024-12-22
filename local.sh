482569b93b30d2d4cc70dbcba97b4982b81ca172
koko-07870 <ko282ko82@gmail.com>
2024-12-22 07:09:44 +0530

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+rm -rf device/samsung
+rm -rf vendor/samsung
+rm -rf hardware/samsung
+rm -rf packages/apps/ViPER4AndroidFX
+rm -rf vendor/extra
+rm -rf kernel/samsung
+rm -rf out/soong/*.glob*
 
+repo init -u repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+
+# local_manifests
+git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
+
+# sync
+/opt/crave/resync.sh
+
+# keys
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
 
-rm -rf out/soong/*.glob*
+
+# Export
+export BUILD_USERNAME=koko-07870
+export BUILD_HOSTNAME=crave
+echo "exported hostname"
+
+# start
 source build/envsetup.sh
 export TARGET_BUILD_GAPPS=true
 lunch yaap_a52q-user
