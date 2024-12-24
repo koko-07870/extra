ed3bf4031717309ca5346bca70a3908447b4470e
koko-07870 <ko282ko82@gmail.com>
2024-12-24 19:14:26 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,17 +2,7 @@
 
 rm -rf .repo/local_manifests
 
-# local_manifests
-git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
-
 # sync
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
-# keys
 rm -rf vendor/yaap/signing/keys
-git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
-
-# start
-source build/envsetup.sh
-lunch yaap_a52q-user
-make installclean && m yaap -j$(nproc --all)
