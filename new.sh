ab81eb7072b25aa3a7d2afff3d8da0e30406c767
koko-07870 <ko282ko82@gmail.com>
2024-12-24 19:18:40 +0530

    Delete new.sh

--- new.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf .repo/local_manifests
-
-# sync
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-
-rm -rf vendor/yaap/signing/keys
