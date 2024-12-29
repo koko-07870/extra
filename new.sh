143296fa5c66c39a6b609ea909c06301d06c43ab
koko-07870 <ko282ko82@gmail.com>
2024-12-30 01:10:33 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+repo init -u https://github.com/kawaaii/pixelos-manifest.git -b fifteen-qpr1 --git-lfs
+
+git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .repo/local_manifests
+
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+
+rm -rf vendor/aosp/signing/keys
+git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
+
+source build/envsetup.sh
+
+breakfast a52q && make installclean && mka bacon -j$(nproc --all)
