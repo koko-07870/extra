e88f1e4eeb18a92dc00122eede296d6dc18b75f8
koko-07870 <ko282ko82@gmail.com>
2024-12-31 00:45:57 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,12 +2,10 @@
 rm -rf .repo/local_manifests
 rm -rf out/soong/*.glob*
 
-git clone https://github.com/koko-07870/local_manifests --depth 1 -b test .repo/local_manifests
+git clone https://github.com/koko-07870/local_manifests --depth 1 -b lineage .repo/local_manifests
 
 repo sync -c -j66 --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
-rm -rf vendor/yaap/signing/keys
-git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
-
+croot
 source build/envsetup.sh
-lunch yaap_a52q-user && m yaap -j$(nproc --all)
+brunch a52q
