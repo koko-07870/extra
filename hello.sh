71056484ea8d6873e005bd116908e40aba9e50c9
koko-07870 <ko282ko82@gmail.com>
2024-12-31 02:11:57 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 rm -rf .repo/local_manifests
 rm -rf out/soong/*.glob*
+
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
 
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b lineage .repo/local_manifests
 
 repo sync -c -j66 --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
 source build/envsetup.sh
-export USE_CCACHE=0
-
+croot
 brunch a52q
