71bcf06622d013d999bd771c3f7f2639c3fa2172
koko-07870 <ko282ko82@gmail.com>
2024-12-31 01:33:57 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -7,4 +7,8 @@ git clone https://github.com/koko-07870/local_manifests --depth 1 -b lineage .re
 repo sync -c -j66 --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
 source build/envsetup.sh
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+
 breakfast a52q && mka bacon
