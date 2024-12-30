de32cfa6a0767dc7396106060f2e4a57e05807e5
koko-07870 <ko282ko82@gmail.com>
2024-12-30 14:12:20 +0530

    Update pos_a52.sh

--- pos_a52.sh
+++ pos_a52.sh
@@ -11,23 +11,17 @@ rm -rf out/soong/*.glob*
 
 repo init -u https://github.com/kawaaii/pixelos-manifest.git -b fifteen-qpr1 --git-lfs
 
-# local_manifests
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .repo/local_manifests
 
-# sync
 /opt/crave/resync.sh
 
-# keys
 rm -rf vendor/aosp/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
 
-
-# Export
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
 echo "exported hostname"
 
-# start
 source build/envsetup.sh
 
 breakfast a52q && make installclean && mka bacon -j$(nproc --all)
