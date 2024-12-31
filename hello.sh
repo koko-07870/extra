7df2f03199ffba8dfe3518f3e0a39886d05abb28
koko-07870 <ko282ko82@gmail.com>
2024-12-31 11:09:15 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,12 +3,13 @@
 rm -rf .repo/local_manifests
 rm -rf out/soong/*.glob*
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
-
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b lineage .repo/local_manifests
 
 repo sync -c -j66 --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
+rm -rf vendor/yaap/signing/keys
+git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
+
 source build/envsetup.sh
-croot
-brunch a52q
+lunch yaap_a52q-user
+m yaap -j$(nproc --all)
