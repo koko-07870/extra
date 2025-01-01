cd0ef66ecf18611d9523879b88a22163d4cbbef2
koko-07870 <ko282ko82@gmail.com>
2025-01-02 01:14:38 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,6 +2,8 @@
 
 rm -rf .repo/local_manifests
 
+repo init -u https://github.com/kawaaii/pixelos-manifest.git -b fifteen-qpr1 --git-lfs
+
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos-a52 .repo/local_manifests
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
