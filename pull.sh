e24446e1f90b305ff1b4cf923eba926b266598b0
koko-07870 <ko282ko82@gmail.com>
2025-01-02 17:53:11 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIRROM="Pixel14/"
+DIRROM="pos/"
 # Check if the directory exists
 if [ -d "$DIRROM" ]; then
     echo "Directory $DIRROM exists."
@@ -8,17 +8,17 @@ if [ -d "$DIRROM" ]; then
     rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
     curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
     echo "--------------cloned crave yaml---------------"
-    crave pull out/target/product/*/*.zip
+    crave pull out/target/product/a52q/PixelOS*.zip
     crave pull out/build_date.txt
     echo "-----done------"
 else
     echo "Directory $DIRROM does not exist. now creating."
-    crave clone create --projectID 82 /crave-devspaces/Pixel14
+    crave clone create --projectID 82 /crave-devspaces/pos
     cd "$DIRROM"
     rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
     curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
     echo "--------------cloned crave yaml---------------"
-    crave pull out/target/product/*/*.zip
+    crave pull out/target/product/a52q/PixelOS*.zip
     crave pull out/build_date.txt
     echo "-----done------"
 fi
