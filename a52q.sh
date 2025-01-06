678d635a3ff5b5cedd34d280fd3949c23185312f
koko-07870 <ko282ko82@gmail.com>
2025-01-06 12:54:17 +0530

    Add files via upload

---
+++ a52q.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+function compile()
+{
+SECONDS=0 # builtin bash timer
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 20G
+TANGGAL=$(date +"%Y%m%d-%H")
+export ARCH=arm64
+export KBUILD_BUILD_HOST=linux-build
+export KBUILD_BUILD_USER="koko"
+clangbin=../clang/bin/clang
+if ! [ -a $clangbin ]; then
+wget -O toolchain.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r522817.tar.gz
+mkdir -p ${PWD}/../clang
+tar -xzf toolchain.tar.gz -C ${PWD}/../clang
+rm -rf toolchain.tar.gz
+fi
+make O=out ARCH=arm64 vendor/pixelos-a52q_defconfig
+PATH="${PWD}/../clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      STRIP=llvm-strip \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump\
+                      CROSS_COMPILE=aarch64-linux-gnu-\
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      LD=ld.lld \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+function zupload()
+{
+zimage=out/arch/arm64/boot/Image.gz
+if ! [ -a $zimage ];
+then
+echo  " Failed To Compile Kernel"
+else
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+git clone --depth=1 https://github.com/koko-07870/AnyKernel3.git -b master AnyKernel3
+cp out/arch/arm64/boot/Image.gz AnyKernel3
+cd AnyKernel3
+zip -r9 ../Spark-2.1-a52q-${TANGGAL}.zip *
+cd ..
+rm -rf AnyKernel3
+fi
+}
+compile
+zupload
