9fba6aee1af5a6be1ff31744e4ae4ba0dd03199d
koko-07870 <ko282ko82@gmail.com>
2025-01-19 00:55:00 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -11,6 +11,9 @@ git clone https://github.com/koko-07870/local_manifests --depth 1 -b yaap .repo/
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
 
+rm -rf packages/apps/FastCharge
+git clone https://github.com/yaap/packages_apps_FastCharge -b fifteen packages/apps/FastCharge
+
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
 echo "exported hostname"
