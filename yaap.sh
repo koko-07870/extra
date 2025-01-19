be374a6e0e5957a899d7a6f364d86e358c779547
koko-07870 <ko282ko82@gmail.com>
2025-01-19 23:02:14 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -10,11 +10,9 @@ git clone https://github.com/koko-07870/local_manifests --depth 1 -b yaap .repo/
 
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
-
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
 echo "exported hostname"
-
 source build/envsetup.sh
 
 lunch yaap_a52q-user && m installclean && m yaap
