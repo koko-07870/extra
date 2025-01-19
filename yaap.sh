8547db80b8051c6b9aeef1c8645d60edc4c949d3
koko-07870 <ko282ko82@gmail.com>
2025-01-19 17:20:20 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -5,7 +5,9 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b yaap .repo/local_manifests
+
 /opt/crave/resync.sh
+
 rm -rf vendor/yaap/signing/keys
 rm -rf .repo/local_manifests
 git clone https://github.com/koko-07870/local_manifests -b tmp .repo/local_manifests
