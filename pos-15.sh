7dfc1b1ee3abcd9d9631825f0f2f7c89b4e5e5c7
koko-07870 <ko282ko82@gmail.com>
2025-01-19 23:21:22 +0530

    Update pos-15.sh

--- pos-15.sh
+++ pos-15.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
+
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos .repo/local_manifests
