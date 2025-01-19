f47d53672c93fcd8c4322953937f90eba26d940c
koko-07870 <ko282ko82@gmail.com>
2025-01-19 12:26:46 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -14,11 +14,6 @@ git clone https://github.com/koko-07870/extra -b tmp vendor/yaap/signing/keys
 rm -rf packages/apps/FastCharge
 git clone https://github.com/yaap/packages_apps_FastCharge -b fifteen packages/apps/FastCharge
 
-rm -rf hardware/qcom-caf/common
-git clone https://github.com/LineageOS/android_hardware_qcom-caf_common -b lineage-22.1 hardware/qcom-caf/common
-
-rm -rf hardware/qcom/display
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-22.1-caf-sm8150 hardware/qcom/display
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
 echo "exported hostname"
