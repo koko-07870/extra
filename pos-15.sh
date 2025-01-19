9417e988252d21d2dbe02ed5e6b5673caefb8ed9
koko-07870 <ko282ko82@gmail.com>
2025-01-19 16:09:32 +0530

    Update pos-15.sh

--- pos-15.sh
+++ pos-15.sh
@@ -5,11 +5,11 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 
 git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos .repo/local_manifests
-
 /opt/crave/resync.sh
-
 rm -rf vendor/aosp/signing/keys
-git clone https://github.com/koko-07870/extra -b tmp vendor/aosp/signing/keys
+rm -rf .repo/local_manifests
+git clone https://github.com/koko-07870/local_manifests -b tmp .repo/local_manifests
+repo sync extra
 
 export BUILD_USERNAME=koko-07870
 export BUILD_HOSTNAME=crave
